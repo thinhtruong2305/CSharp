e166f9b5da93c2280fd862443b7ea607eea52ab8
thinhtruong2305 <56267717+thinhtruong2305@users.noreply.github.com>
2022-06-06 14:25:05 +0700

    Add hai bài học mới
    1. CkEditor và UploadFile
    2. Razor

---
+++ Công nghệ Web/BaiHoc/UploadFile_CKEditor/UploadFile_CKEditor/Controllers/CkEditorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UploadFile_CKEditor.Controllers
+{
+    public class CkEditorController : Controller
+    {
+        // GET: CkEditor
+        public ActionResult Index()
+        {
+            Models.CkEditor ckEditor = new Models.CkEditor();
+            return View(ckEditor);
+        }
+    }
+}
---
+++ Công nghệ Web/BaiHoc/UploadFile_CKEditor/UploadFile_CKEditor/Models/CkEditor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UploadFile_CKEditor.Models
+{
+    public class CkEditor
+    {
+        public String description { get; set; }
+    }
+}
